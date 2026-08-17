c9abf2c77a72d305fb7e67ed6d580e8971a44bd1
MSiWPsP <2126163242@qq.com>
2026-08-17 18:27:56 +0800

    护理项目前端代码开发及ui优化

--- src/components/RuoYi/Git/index.vue
+++ src/components/RuoYi/Git/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <svg-icon icon-class="github" @click="goto" />
-  </div>
+   <div>
+      <svg-icon icon-class="github" @click="goto" />
+   </div>
 </template>
 
 <script setup>
-const url = ref('https://gitee.com/y_project/RuoYi-Vue');
+const url = ref('https://github.com/MSiWPsP');
 
 function goto() {
-  window.open(url.value)
+   window.open(url.value)
 }
 </script>
